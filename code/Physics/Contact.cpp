94c73d457b6aef276995c8cfb5e867cf21fa1f21
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-05-02 01:51:54 +0900

    Update Contact.cpp
    apply total linear impulse

--- code/Physics/Contact.cpp
+++ code/Physics/Contact.cpp
@@ -12,21 +12,38 @@ void ResolveContact( contact_t & contact ) {
 	Body* bodyA = contact.bodyA;
 	Body* bodyB = contact.bodyB;
 
-	const float invMassA = bodyA->m_invMass;
-	const float invMassB = bodyB->m_invMass;
+	const Vec3 pointOnA = contact.ptOnA_WorldSpace;
+	const Vec3 pointOnB = contact.ptOnB_WorldSpace;
 
 	const float elasticityA = bodyA->m_elasticity;
 	const float elasticityB = bodyB->m_elasticity;
 	const float elasticity = elasticityA * elasticityB;	// [0,1]
 
-	// calculate the collision impulse
-	const Vec3& normalizedVector = contact.normal;
-	const Vec3& velocityAminusB = bodyA->m_linearVelocity - bodyB->m_linearVelocity;
-	const float impulseJ = -(1.0f + elasticity) * velocityAminusB.Dot(normalizedVector) / (invMassA + invMassB);
-	const Vec3 impulseVector = normalizedVector * impulseJ;
+	const float invMassA = bodyA->m_invMass;
+	const float invMassB = bodyB->m_invMass;
 
-	bodyA->ApplyImpulseLinear(impulseVector * 1.0f);
-	bodyB->ApplyImpulseLinear(impulseVector * -1.0f);
+	const Mat3 invWorldInertiaA = bodyA->GetInverseInertiaTensorWorldSpace();
+	const Mat3 invWorldInertiaB = bodyA->GetInverseInertiaTensorWorldSpace();
+
+	const Vec3 normalizedVector = contact.normal;
+
+	const Vec3 comToPointA = pointOnA - bodyA->GetCenterOfMassWorldSpace();
+	const Vec3 comToPointB = pointOnB - bodyB->GetCenterOfMassWorldSpace();
+
+	const Vec3 angularJA = (invWorldInertiaA * comToPointA.Cross(normalizedVector)).Cross(comToPointA);
+	const Vec3 angularJB = (invWorldInertiaB * comToPointB.Cross(normalizedVector)).Cross(comToPointB);
+	const float angularFactor = (angularJA + angularJB).Dot(normalizedVector);
+
+	// get the world space velocity of the motion and rotation
+	const Vec3 totalVelocityA = bodyA->m_linearVelocity + bodyA->m_angularVelocity.Cross(comToPointA);
+	const Vec3 totalVelocityB = bodyB->m_linearVelocity + bodyB->m_angularVelocity.Cross(comToPointB);
+
+	// apply impulses
+	const Vec3 totalRelativeVelocity = totalVelocityA - totalVelocityB;
+	const float totalImpulseScalar = (1.0f + elasticity) * totalRelativeVelocity.Dot(normalizedVector) / (invMassA + invMassB + angularFactor);
+	const Vec3 totalImpulseVector = normalizedVector * totalImpulseScalar;
+	bodyA->ApplyImpulse(pointOnA, totalImpulseVector * -1.0f);
+	bodyB->ApplyImpulse(pointOnB, totalImpulseVector * 1.0f);
 
 
 	// let's also move our colliding objects to just outside of each other
