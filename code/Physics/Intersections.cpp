d7af48a56a960d36e5bfa9247942201f124851e1
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-05-01 13:37:46 +0900

    Update Intersections.cpp
    implement interset()

--- code/Physics/Intersections.cpp
+++ code/Physics/Intersections.cpp
@@ -9,8 +9,24 @@
 Intersect
 ====================================================
 */
-bool Intersect( Body * bodyA, Body * bodyB, contact_t & contact ) {
-	// TODO: Add Code
+bool Intersect( Body *bodyA, Body *bodyB, contact_t &contact ) {
+	contact.bodyA = bodyA;
+	contact.bodyB = bodyB;
+
+	const Vec3 vectorAB = bodyB->m_position - bodyA->m_position;
+	contact.normal = vectorAB;
+	contact.normal.Normalize();
+
+	const ShapeSphere* sphereA = (const ShapeSphere*)bodyA->m_shape;
+	const ShapeSphere* sphereB = (const ShapeSphere*)bodyB->m_shape;
+
+	contact.ptOnA_WorldSpace = bodyA->m_position + contact.normal * sphereA->m_radius;
+	contact.ptOnB_WorldSpace = bodyB->m_position + contact.normal * sphereB->m_radius;
+
+	const float radiusAB = sphereA->m_radius + sphereB->m_radius;
+	const float lengthSquared = vectorAB.GetLengthSqr();
+	if (lengthSquared <= (radiusAB * radiusAB))
+		return true;
 
 	return false;
 }
