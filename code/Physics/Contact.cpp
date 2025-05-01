2dd27f47a283c12ed8e0ccea87f95e62921817b1
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-05-01 13:58:42 +0900

    Update Contact.cpp
    implement ResolveContact()

--- code/Physics/Contact.cpp
+++ code/Physics/Contact.cpp
@@ -9,5 +9,17 @@ ResolveContact
 ====================================================
 */
 void ResolveContact( contact_t & contact ) {
-	// TODO: Add Code
+	Body* bodyA = contact.bodyA;
+	Body* bodyB = contact.bodyB;
+
+	bodyA->m_linearVelocity.Zero();
+	bodyB->m_linearVelocity.Zero();
+
+	// let's also move our colliding objects to just outside of each other
+	const float proportionOfA = bodyA->m_invMass / (bodyA->m_invMass + bodyB->m_invMass);
+	const float proportionOfB = bodyB->m_invMass / (bodyA->m_invMass + bodyB->m_invMass);
+
+	const Vec3 vectorBtwTwoContactPoints = contact.ptOnB_WorldSpace - contact.ptOnA_WorldSpace;
+	bodyA->m_position += vectorBtwTwoContactPoints * proportionOfA;
+	bodyB->m_position -= vectorBtwTwoContactPoints * proportionOfB;
 }
