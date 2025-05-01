223b23c4d90bdab2ad8a293c94ce3d1ed752d741
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-05-01 17:35:35 +0900

    Update Body.h
    add e

--- code/Physics/Body.h
+++ code/Physics/Body.h
@@ -23,7 +23,9 @@ public:
 	Vec3		m_position;
 	Quat		m_orientation;
 	Vec3		m_linearVelocity;
+
 	float		m_invMass;
+	float		m_elasticity;
 	Shape*		m_shape;
 
 	Vec3 GetCenterOfMassWorldSpace() const;
@@ -31,5 +33,5 @@ public:
 	Vec3 WorldSpaceToBodySpace(const Vec3& pt) const;
 	Vec3 BodySpaceToWorldSpace(const Vec3& pt) const;
 
-	void ApplyImpulseLinear(const Vec3& impulse);
+	void ApplyImpulseLinear(const Vec3& impluse);
 };
