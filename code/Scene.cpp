dd4598b352773a69854f7e5c381fa10aab8bb6a1
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-05-03 15:41:18 +0900

    Update Scene.cpp
    apply toi

--- code/Scene.cpp
+++ code/Scene.cpp
@@ -20,8 +20,8 @@ Scene::~Scene
 ====================================================
 */
 Scene::~Scene() {
-	for ( int i = 0; i < m_bodies.size(); i++ ) {
-		delete m_bodies[ i ].m_shape;
+	for (int i = 0; i < m_bodies.size(); i++) {
+		delete m_bodies[i].m_shape;
 	}
 	m_bodies.clear();
 }
@@ -32,8 +32,8 @@ Scene::Reset
 ====================================================
 */
 void Scene::Reset() {
-	for ( int i = 0; i < m_bodies.size(); i++ ) {
-		delete m_bodies[ i ].m_shape;
+	for (int i = 0; i < m_bodies.size(); i++) {
+		delete m_bodies[i].m_shape;
 	}
 	m_bodies.clear();
 
@@ -47,13 +47,22 @@ Scene::Initialize
 */
 void Scene::Initialize() {
 	Body body;
-	body.m_position = Vec3(0, 0, 1);
+	body.m_position = Vec3(-3, 0, 3);
 	body.m_orientation = Quat(0, 0, 0, 1);
-	body.m_linearVelocity = Vec3(1, 0, 0);
+	body.m_linearVelocity = Vec3(10, 0, 0);
 	body.m_invMass = 1.0f;
 	body.m_elasticity = 0.0f;
 	body.m_friction = 0.5f;
 	body.m_shape = new ShapeSphere(1.0f);
+	m_bodies.push_back(body);
+
+	body.m_position = Vec3(0, 0, 3);
+	body.m_orientation = Quat(0, 0, 0, 1);
+	body.m_linearVelocity = Vec3(0, 0, 0);
+	body.m_invMass = 0.0f;
+	body.m_elasticity = 0.0f;
+	body.m_friction = 0.5f;
+	body.m_shape = new ShapeSphere(0.5f);
 	m_bodies.push_back(body);
 
 	// Add a ground sphere that won't fall under the influence of gravity
@@ -72,7 +81,8 @@ void Scene::Initialize() {
 Scene::Update
 ====================================================
 */
-void Scene::Update( const float deltaSecond ) {
+void Scene::Update(const float deltaSecond) {
+	// Gravity impulse
 	for (int currentBodyIndex = 0; currentBodyIndex < m_bodies.size(); ++currentBodyIndex) {
 		Body* currentBody = &m_bodies[currentBodyIndex];
 
@@ -81,22 +91,59 @@ void Scene::Update( const float deltaSecond ) {
 		currentBody->ApplyImpulseLinear(impulseGravity);
 	}
 
+	int numContacts = 0;
+	const int maxContacts = m_bodies.size() * m_bodies.size();
+	contact_t* contacts = reinterpret_cast<contact_t*>(alloca(sizeof(contact_t) * maxContacts));
+
 	// check for collisions with other bodies
 	for (int currentBodyIndex = 0; currentBodyIndex < m_bodies.size(); ++currentBodyIndex) {
 		for (int currentBodyIndexNested = currentBodyIndex + 1; currentBodyIndexNested < m_bodies.size(); ++currentBodyIndexNested) {
 			Body* bodyA = &m_bodies[currentBodyIndex];
 			Body* bodyB = &m_bodies[currentBodyIndexNested];
-		
+
 			// skip body pairs with infinite mass
 			if (0.0f == bodyA->m_invMass && 0.0f == bodyB->m_invMass)
 				continue;
 
 			contact_t contact;
-			if (Intersect(bodyA, bodyB, contact))
-				ResolveContact(contact);
+			if (Intersect(bodyA, bodyB, deltaSecond, contact)) {
+				contacts[numContacts] = contact;
+				++numContacts;
+			}
 		}
 	}
 
-	for (int currentBodyIndex = 0; currentBodyIndex < m_bodies.size(); ++currentBodyIndex)
-		m_bodies[currentBodyIndex].Update(deltaSecond);
+	// sort TOI from earliest to latest
+	if (numContacts > 1)
+		qsort(contacts, numContacts, sizeof(contact_t), CompareContacts);
+
+	// resolve collisions
+	// note that there’s no recalculation of earlier collisions for later ones to improve performance.
+	// thus, while the first collision is handled correctly, later collisions may be processed improperly if they are related to the earlier collisions.
+	float accumulatedTime = 0.0f;
+	for (int currentContactIndex = 0; currentContactIndex < numContacts; ++currentContactIndex) {
+		contact_t& contact = contacts[currentContactIndex];
+		const float deltaTime = contact.timeOfImpact - accumulatedTime;
+
+		Body* bodyA = contact.bodyA;
+		Body* bodyB = contact.bodyB;
+
+		// skip body pairs with infinite mass
+		if (0.0f == bodyA->m_invMass && 0.0f == bodyB->m_invMass)
+			continue;
+
+		// position update
+		for (int currentBodyIndex = 0; currentBodyIndex < m_bodies.size(); ++currentBodyIndex)
+			m_bodies[currentBodyIndex].Update(deltaTime);
+
+		ResolveContact(contact);
+		accumulatedTime += deltaTime;
+	}
+
+	// update the positions for the rest of this frame's time
+	const float timeRemaining = deltaSecond - accumulatedTime;
+	if (timeRemaining > 0.0f) {
+		for (int currentBodyIndex = 0; currentBodyIndex < m_bodies.size(); ++currentBodyIndex)
+			m_bodies[currentBodyIndex].Update(timeRemaining);
+	}
 }
