b3e403c8c9152ee3d653e94d2eeea715eb50f318
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-05-07 00:15:43 +0900

    Update OffscreenRenderer.h
    add isDebug to DrawOffscreen()

--- code/Renderer/OffscreenRenderer.h
+++ code/Renderer/OffscreenRenderer.h
@@ -10,6 +10,5 @@ struct RenderModel;
 
 bool InitOffscreen( DeviceContext * device, int width, int height );
 bool CleanupOffscreen( DeviceContext * device );
-
 void DrawDebugAxes(VkCommandBuffer cmdBuffer, DeviceContext* device, const RenderModel& model, Buffer* uniforms);
-void DrawOffscreen( DeviceContext * device, int cmdBufferIndex, Buffer * uniforms, const RenderModel * renderModels, const int numModels );
+void DrawOffscreen( DeviceContext * device, int cmdBufferIndex, Buffer * uniforms, const RenderModel * renderModels, const int numModels, bool isDebug );
