6c97afbe17d6416f1cc863e64841d61f103588e2
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-05-06 22:41:08 +0900

    Update OffscreenRenderer.h
    add a function for visual debugger

--- code/Renderer/OffscreenRenderer.h
+++ code/Renderer/OffscreenRenderer.h
@@ -10,4 +10,5 @@ struct RenderModel;
 bool InitOffscreen( DeviceContext * device, int width, int height );
 bool CleanupOffscreen( DeviceContext * device );
 
+void DrawDebugAxes(VkCommandBuffer cmdBuffer, DeviceContext* device, const RenderModel& model, Buffer* uniforms);
 void DrawOffscreen( DeviceContext * device, int cmdBufferIndex, Buffer * uniforms, const RenderModel * renderModels, const int numModels );
